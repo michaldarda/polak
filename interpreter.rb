32fb86e473b2f3976226c69d0e79a35f3b04664a
Michał Darda <michaldarda@gmail.com>
2013-11-12 23:50:05 +0100

    Polak version ruby style

--- interpreter.rb
+++ interpreter.rb
@@ -2,16 +2,16 @@ puts "Witaj w interpreterze jezyka Polak (c) 2013 by Michal Darda (MIT)"
 require_relative 'evaluator'
 require_relative 'syntax'
 require_relative 'parser'
+require_relative 'version'
 
 environment = {}
 
 while(true)
   STDOUT.flush
-  print ">> "
+  print "#{POLAK_VERSION} >> "
 begin
-  puts PolakParser.new.parse(gets.chomp!).to_ast.evaluate(environment)
+  puts "#=> #{PolakParser.new.parse(gets.chomp!).to_ast.evaluate(environment)}"
 rescue Exception => e
-  print e.message
+  puts e.message
 end
-  puts
 end
---
+++ version.rb
@@ -0,0 +1,1 @@
+POLAK_VERSION = '0.0.1'
