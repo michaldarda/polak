488f884d90710b3f533fda97e54574123ac7d0f2
Michał Darda <michaldarda@gmail.com>
2014-01-25 18:23:45 +0100

    Refactoring

--- evaluator/function_call.rb
+++ evaluator/function_call.rb
@@ -4,28 +4,40 @@ require_relative '../syntax/function_call'
 class FunctionCall
   def evaluate(environment)
     function = environment.fetch(name)
-
-    # ["x", "y", "z" ...]
     formal = function.fetch(:formal)
 
-    # evaluate each parameter in current environment
-    evaluated_actual = actual.map do |param|
-      param.evaluate(environment)
-    end
-
     if formal.size != actual.size
-      raise RuntimeError, "#{name}: Oczekiwano #{formal.count} argumentów, dostałem #{actual.count}"
-    end
-
-    # map formal parameters to the actual ones
-    parameters = {}
-    formal.each_with_index do |f, i|
-      parameters.merge!({ f.to_sym => evaluated_actual[i] })
+      raise "#{name}: Oczekiwano #{formal.count} argumentów,
+        dostałem #{actual.count}"
     end
 
     function_body        = function.fetch(:body)
     function_environment = function.fetch(:environment)
 
-    function_body.evaluate(function_environment.merge(parameters).merge(name => function))
+    parameters = map_formal_to_evaluated_actual(formal, actual, environment)
+
+    current_env = function_environment.merge(parameters)
+      .merge(name => function)
+
+    function_body.evaluate(current_env)
+  end
+
+  protected
+
+  # evaluate each parameter in current environment
+  def evaluate_actual(actual, environment)
+    actual.map do |param|
+      param.evaluate(environment)
+    end
+  end
+
+  def map_formal_to_evaluated_actual(formal, actual, environment)
+    evaluated_actual = evaluate_actual(actual, environment)
+
+    parameters = {}
+    formal.each_with_index do |f, i|
+      parameters.merge!(f.to_sym => evaluated_actual[i])
+    end
+    parameters
   end
 end
